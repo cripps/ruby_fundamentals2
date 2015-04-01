b81a4931689fc5cba6c63faccde7e8fe7f0e0234
cripps <rob.cripps@gmail.com>
2015-04-01 16:21:19 -0400

    Exercise 7 complete

--- exercise6.rb
+++ exercise6.rb
@@ -1,2 +1,27 @@
+
+def print(list)
+  list.each { |item| puts "* " + item }
+end
+
 grocery_list = ["kale", "tomato", "avocado", "cucumber", "feta", "spinach"]
-grocery_list.each { |item| puts "* " + item }
+print(grocery_list)
+
+grocery_list << "rice"
+print(grocery_list)
+
+puts grocery_list.length
+
+if grocery_list.include?("bananas")
+  puts "You have bananas!"
+else
+  puts "You need to pick up bananas!"
+end
+
+puts grocery_list[1]
+print(grocery_list)
+
+grocery_list.sort!
+print(grocery_list)
+
+grocery_list.delete("kale") #can't find kale
+print(grocery_list)
